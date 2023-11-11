60f040fda2fd5a9bc14c2090a6caa81c8db70681
celeryrice <ameypadvekar2001@gmail.com>
2023-11-11 20:39:13 +0530

    first commot

---
+++ app/profile/page.jsx
@@ -0,0 +1,66 @@
+"use client"
+import React from 'react'
+import { useState,useEffect } from 'react'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
+
+import Profile from '@components/Profile'
+
+const MyProfile = () => {
+    const router = useRouter();
+    const {data:session} = useSession();
+    const [posts,setPosts] = useState([]);
+    useEffect(()=>{
+        const fetchPosts = async ()=>{
+          const response = await fetch(`/api/users/${session?.user.id}/posts`);
+          const data = await response.json();
+          setPosts(data);
+        }
+        if(session?.user.id) fetchPosts();
+      },[]);
+    const handleEdit = (post)=>{
+        router.push(`/update-prompt?id=${post._id}`);
+    }
+
+    const handleDelete = async (post)=>{
+        const hasConfirmed = confirm("Are you sure you want to delete this prompt?");
+
+        if(hasConfirmed){
+          try {
+            await fetch(`/api/prompt/${post._id.toString()}`,{
+              method:'delete'
+            })
+            const filteredPosts = new posts.filter((p)=> p._id !== post._id);
+            console.log(filteredPosts);
+            setPosts((prev)=>{
+              prev.filter((p)=> p._id !== post._id);
+            });
+            
+          } catch (error) {
+            console.log(error);
+          }
+        }
+        router.push("/");
+    }
+    useEffect(()=>{
+    const fetchPosts = async ()=>{
+      const response = await fetch('api/prompt');
+      const data = await response.json();
+      setPosts(data);
+    }
+    console.log(posts)
+    fetchPosts();
+  },[]);
+
+  return (
+    <Profile
+        name="My"
+        desc="Welcome to your personalized"
+        data={posts}
+        handleEdit={handleEdit}
+        handleDelete={handleDelete}
+    />
+  )
+}
+
+export default MyProfile
